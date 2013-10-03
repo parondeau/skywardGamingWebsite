8f9a51c8ca5f5a1690d5a1996297cd00c8b868cc
Paul Rondeau <paulrondeau07@gmail.com>
2013-10-02 23:23:06 -0400

    realized i am dumb and fixed my ahcks

--- public/js/main.js
+++ public/js/main.js
@@ -2,32 +2,17 @@ var scrollingTop = false;
 var scrollingAbout = false;
 var scrollingContact = false;
 function scrollToTop(){
-	$("#home").click(function() {
-   	 	if (scrollingTop) return;
-   	 	$('html, body').animate({
-    		scrollTop: 0
-    	}, 500);
-		scrollingTop = true;
-	});
-	scrollingTop = false;
+ 	$('html, body').animate({
+		scrollTop: 0
+	}, 500);
 }
 function scrollToAbout(){
-	$("#aboutButton").click(function() {
-   	 	if (scrollingAbout) return;
-   	 	$('html, body').animate({
-    		scrollTop: $("#aboutSection").offset().top - 50
-    	}, 500);
-		scrollingAbout = true;
-	});
-	scrollingAbout = false;
+ 	$('html, body').animate({
+		scrollTop: $("#aboutSection").offset().top - 50
+	}, 500);
 }
 function scrollToContact(){
-	$("#contactButton").click(function() {
-   	 	if (scrollingContact) return;
-   	 	$('html, body').animate({
+ 	$('html, body').animate({
     		scrollTop: $("#contactSection").offset().top - 50
-    	}, 500);
-    	scrollingContact = true;
-	});
-	scrollingContact = false;
+	}, 500);
 }
