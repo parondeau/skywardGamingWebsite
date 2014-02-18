7a7c74962a9a7b564bd240b8c0193d157a3938b1
Paul Rondeau <paulrondeau07@gmail.com>
2014-02-18 00:21:53 -0500

    complete redo of website front page near completion, links set up and working

--- config.ru
+++ config.ru
@@ -1,13 +1,13 @@
-use Rack::Static, 
-  :urls => ["/images", "/js", "/css", "/fonts"],
+use Rack::Static,
+  :urls => ["/images", "/js", "/css", "/fonts", "templates"],
   :root => "public"
 
 run lambda { |env|
   [
-    200, 
+    200,
     {
-      'Content-Type'  => 'text/html', 
-      'Cache-Control' => 'public, max-age=86400' 
+      'Content-Type'  => 'text/html',
+      'Cache-Control' => 'public, max-age=86400'
     },
     File.open('public/index.html', File::RDONLY)
   ]
--- public/js/main.js
+++ public/js/main.js
@@ -1,18 +1,34 @@
-var scrollingTop = false;
-var scrollingAbout = false;
-var scrollingContact = false;
-function scrollToTop(){
- 	$('html, body').animate({
-		scrollTop: 0
-	}, 500);
-}
-function scrollToAbout(){
- 	$('html, body').animate({
-		scrollTop: $("#aboutSection").offset().top - 50
-	}, 500);
-}
-function scrollToContact(){
- 	$('html, body').animate({
-    		scrollTop: $("#contactSection").offset().top - 50
-	}, 500);
+function onLoad(){
+	$("#navHome").on("click", function(){
+		$(".mainBody > *").removeClass("show");
+		$(".mainSection").addClass("show");
+	});
+	$("#navGame").on("click", function(){
+		$(".mainBody > *").removeClass("show");
+		$(".gameSection").addClass("show");
+	});
+	$("#navStory").on("click", function(){
+		$(".mainBody > *").removeClass("show");
+		$(".historySection").addClass("show");
+	});
+	$("#navContact").on("click", function(){
+		$(".mainBody > *").removeClass("show");
+		$(".contactSection").addClass("show");
+	});
+	$(".business-header").on("click", function(){
+		$(".mainSection").removeClass("show");
+		$(".gameSection").addClass("show");
+	});
+	$("#mainGameSection").on("click", function(){
+		$(".mainSection").removeClass("show");
+		$(".gameSection").addClass("show");
+	});
+	$("#mainHistorySection").on("click", function(){
+		$(".mainSection").removeClass("show");
+		$(".historySection").addClass("show");
+	});
+	$("#mainContactSection").on("click", function(){
+		$(".mainBody > *").removeClass("show");
+		$(".contactSection").addClass("show");
+	});
 }
